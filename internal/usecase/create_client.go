5db27259c818ebd25b03f8779587f36544e3e1c4
dioner <dionerweiss@gmail.com>
2023-05-08 21:59:17 -0300

    create client use case

---
+++ internal/usecase/create_client.go
@@ -0,0 +1,52 @@
+package createClient
+
+import (
+	"time"
+
+	"github.com/dionerweiss/fc-ms-wallet/internal/entity"
+	"github.com/dionerweiss/fc-ms-wallet/internal/gateway"
+)
+
+type CreateClientInputDTO struct {
+	Name  string
+	Email string
+}
+
+type CreateClientUseCase struct {
+	ClientGateway gateway.ClientGateway
+}
+
+type CreateClientOutputDTO struct {
+	ID        string
+	Name      string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func NewCreateClientUseCase(clientGateway gateway.ClientGateway) *CreateClientUseCase {
+	return &CreateClientUseCase{
+		ClientGateway: clientGateway,
+	}
+}
+
+func (uc *CreateClientUseCase) Execute(input CreateClientInputDTO) (*CreateClientOutputDTO, error) {
+	client, err := entity.NewClient(input.Name, input.Email)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = uc.ClientGateway.Save(client)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateClientOutputDTO{
+		ID:        client.ID,
+		Name:      client.Name,
+		Email:     client.Email,
+		CreatedAt: client.CreatedAt,
+		UpdatedAt: client.UpdatedAt,
+	}, nil
+}
