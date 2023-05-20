da0aca6e6d9b59dbe92b8a7c821fd2692b616770
Dioner <dionerweiss@gmail.com>
2023-05-20 00:48:15 -0300

    add dispatch and remove methods to event dispatcher

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -1,6 +1,9 @@
 package events
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
 
@@ -39,4 +42,28 @@ func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) 
 		}
 	}
 	return false
+}
+
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
+		for _, handler := range handlers {
+			wg.Add(1)
+			go handler.Handle(event, wg)
+		}
+		wg.Wait()
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for i, h := range ed.handlers[eventName] {
+			if h == handler {
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
 }
